2d611f2c10681301ccd6e821f4b44bd3d98a46a0
BYOUL JUNG <129057067+JungByoul@users.noreply.github.com>
2023-07-14 14:33:18 +0900

    footer기본구성

--- src/style/global/header/body.jsx
+++ src/style/global/header/body.jsx
@@ -1,14 +1,29 @@
 import Header from './style';
+import {Link} from 'react-router-dom';
 
 function Body() {
     return (
         <div>
             <Header>
             <h1>
-                Mega little lion
+                {/* 홈 로딩 버튼 만들기 */}
+
+                <a id= 'homebutton' href="/">Mega little lion </a>
 
             </h1>
+
+            <h2>
+                <Link id = 'loginbutton'>
+                    로그인
+                </Link>
+                <Link id = 'signupbutton'>
+                    회원가입
+                </Link>
+
+            </h2>
+
             </Header>
+            
 
         </div>
     );
--- src/style/global/header/style.jsx
+++ src/style/global/header/style.jsx
@@ -5,15 +5,37 @@ const Header = styled.h1`
 
     background-color: #FFD732;
 
-
-
     h1{
         margin: 0px;
         padding: 0px;
+
+    };
+
+    #homebutton{
         color: #666565;
         font-size: 30px;
 
+        text-decoration: none;
+        margin-left: 15px;
+
+    };
+
+    h2{
+        margin: 0px;
+
+        display: flex;
+        flex-direction: column;
+        align-items: flex-end;
+
+        margin-right: 15px;
+
+        font-size: 20px;
     }
+    #signupbutton, #loginbutton{
+        text-decoration: none;
+        color: #666565;
+    }
+
 `
 
 export default Header;
