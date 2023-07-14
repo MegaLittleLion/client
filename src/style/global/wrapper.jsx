58df1ef2fed28805fb7b8cdad28a68b232a7c25a
양민석 <kurooru98@gmail.com>
2023-07-14 11:22:17 +0900

    wrapper correction

--- src/style/global/wrapper.jsx
+++ src/style/global/wrapper.jsx
@@ -1,8 +1,10 @@
 import styled from "styled-components";
 
-export const Wrapper = styled.div`
+const Wrapper = styled.div`
     margin: 0 auto;
     padding: 0;
     text-decoration: none;
     max-width: 1280px;
 `
+
+export default Wrapper;
