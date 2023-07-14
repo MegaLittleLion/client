03968d60cec347ccc6212f4cb07d5305b2f66915
BYOUL JUNG <129057067+JungByoul@users.noreply.github.com>
2023-07-14 15:00:57 +0900

    수정중

--- src/pages/main/body.jsx
+++ src/pages/main/body.jsx
@@ -1,15 +1,42 @@
-import {Title} from './style'
+import { Title } from './style'
 import { getMovies } from '../../apis/mainApis/apis';
 
-function Body(){
+function Body() {
     getMovies();
 
 
-    return(
-        
-        <>
-        
-        </>
+    return (
+
+        <Title>
+
+            <div id='mainpage_total_box'>
+                <h1 id='mainpage_recent_movie_box'>
+                    <p id='mainpage_recent_movie_article'>최신영화</p>
+
+                    <div id= 'mainpage_api'>
+
+                    </div>
+
+
+                </h1>
+
+                <h2 id='mainpage_popular_movie_box'>
+                    <p id='mainpage_popular_movie_article'>인기영화</p>
+
+                    a
+
+                </h2>
+
+
+
+
+
+
+            </div>
+
+
+        </Title>
+
 
 
     );
--- src/pages/main/style.jsx
+++ src/pages/main/style.jsx
@@ -4,7 +4,25 @@ import styled from 'styled-components';
 
 export const Title = styled.h1`
 
+    #mainpage_total_box{
 
+        margin: 50px;
+
+    }
+    #mainpage_recent_movie_box{
+        margin-bottom: 5px;
+
+    }
+    #mainpage_recent_movie_article{
+
+    }
+    #mainpage_second_box{
+        margin-top: 5px;
+
+    }
+    #mainpage_popular_movie_box{
+
+    }
 
 `
 
