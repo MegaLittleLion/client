8d1f9f40f64986929b94dfe225110dde2bf7ca66
Seoheeda <1015aliciay@gmail.com>
2023-07-14 15:35:20 +0900

    details

--- src/pages/detail/style.jsx
+++ src/pages/detail/style.jsx
@@ -1,9 +1,26 @@
 import styled from 'styled-components';
 
-export const Title = styled.h1`
+export const Whole = styled.div`
+    font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+    margin-top: 50px;
+    margin-left: 30px;
+    margin-right: 30px
 
 `
 
-export const Poster = styled.img`
+export const Title = styled.div`
+
+`
+
+
+export const Infos = styled.div`
+    display:flex;
+    div{
+        margin-left:50px;
+    };
+`
+
+export const Staff = styled.div`
+    font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
     
 `
