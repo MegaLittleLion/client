0d1577c79dd184c4e176c7edbeb4858127e0b024
양민석 <kurooru98@gmail.com>
2023-07-14 10:59:05 +0900

    routing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Main from "./pages/main";
+import Detail from "./pages/detail";
+import Signup from "./pages/signup";
+import Login from "./pages/login";
 function App() {
   return (
-    <></>
+    <div>
+    <Router>
+      <Routes>
+        <Route path={`/`} element={<Main />}></Route>
+        <Route path={`/detail`} element={<Detail />}></Route>
+        <Route path={`/signup`} element={<Signup />}></Route>
+        <Route path={`/login`} element={<Login />}></Route>
+      </Routes>
+    </Router>
+    </div>
   );
 }
 
